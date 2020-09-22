8ff0fcb9d348e67bbc16dfb9e83b93e83f1c248e
jamesho22 <8.james.ho@gmail.com>
2020-09-22 18:27:31 -0400

    can drive straight for n meters, cannot turn 90 reliably

--- warmup_neato/scripts/drive_square.py
+++ warmup_neato/scripts/drive_square.py
@@ -4,6 +4,7 @@
 from std_msgs.msg import Int8MultiArray
 from geometry_msgs.msg import Twist, Vector3, Pose
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
 
 import rospy
 import time
@@ -15,40 +16,60 @@ class Square:
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.twist = Twist(Vector3(1,0,0), Vector3(0,0,0))
         self.pub.publish(self.twist)
-        self.starting_point = (0,0,0)
-        self.sidelength = 1
+        self.sidelength = 2
         self.coef = 1
         self.sub = rospy.Subscriber('/odom', Odometry, self.response)
-
+        self.subImu = rospy.Subscriber('/imu', Imu, self.responseImu)
+        self.current_position = (0, 0)
+        self.setpoint = (0, 0)
+        self.motorSpeed = 0 
+       
 
     
 
 
     def run_odom(self):
-
         # setup sub to odom / imu data
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
-            if distance_driven >= self.sidelength:
-                # print('turning')
-                self.turn()
-            else: 
-                # print('driving straight')
-                self.drive_straight(distance_driven)
+            d = self.get_distance()
+            if d < self.sidelength:
+                self.twist.angular.z = 0
+                # keep driving forward
+                # get motor speed
+                error = abs((d**2-self.sidelength**2))**0.5
+                self.motorSpeed = error
+                self.currentAngle = 0
+                
+                
+            else:
+                self.twist.linear.x = 0
+                # calculate the angle to increase by
+                self.twist.angular.z = 1
+                
+                    
+                
+                # stop driving, turn right 90 deg, set new setpoint as current pos
+            self.pub.publish(self.twist)
             r.sleep()
         
+    def responseImu(self, message):
+        self.currentAngle = message.orientation.w
 
     def response(self, message):
-        print(message.pose.pose.position) 
-        # calc distance driven 
-        distance_driven = self.get_distance(message.pose.pose.position)
-        # print('distance: ', distance_driven)
-        self.current_position = (message.pose.pose.position.x,message.pose.pose.position.y,0)
+        '''
+        updates the current position class attribute
+        '''
+        self.current_position = (message.pose.pose.position.x, message.pose.pose.position.y,0)
+        
 
 
-    def get_distance(self, position):
+    def get_distance(self):
+        '''
+        compares the current position class attribute to the last setpoint to calculate distance from it
+        '''
         # grab euclidean distance from starting point to current position 
-         return ((position.x-self.starting_point[0])**2+(position.y-self.starting_point[1])**2)**.5
+        return ((self.current_position[0]-self.setpoint[0])**2+(self.current_position[1]-self.setpoint[1])**2)**.5
 
 
     def get_speed(self, distance_driven):
