12a79b5198b1ac7634f9795aa863ad557802f62e
Adi <aramachandran@olin.edu>
2020-09-22 22:27:44 -0500

    made some minor changes to drive_square

--- warmup_neato/scripts/drive_square.py
+++ warmup_neato/scripts/drive_square.py
@@ -11,18 +11,28 @@ import time
 
 class Square:
     def __init__(self):
-        # init node         
+        # init node
         rospy.init_node('square_drive')
+
+        # publish first speed at 0
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        self.twist = Twist(Vector3(1,0,0), Vector3(0,0,0))
+        self.twist = Twist(Vector3(0,0,0), Vector3(0,0,0))
         self.pub.publish(self.twist)
+
+        # set some coef's
         self.sidelength = 2
         self.coef = 1
-        self.sub = rospy.Subscriber('/odom', Odometry, self.response)
-        self.subImu = rospy.Subscriber('/imu', Imu, self.responseImu)
-        self.current_position = (0, 0)
-        self.setpoint = (0, 0)
+        self.coef_a = 1
+
+        # set your subscribers
+        self.sub = rospy.Subscriber('/odom', Odometry, self.response_odom)
+        self.subImu = rospy.Subscriber('/imu', Imu, self.response_imu)
+
+        # set data
+        self.set_point = (0, 0)
         self.motorSpeed = 0 
+        self.current_position = (0, 0)
+        self.currentAngle = 0
        
 
     
@@ -34,29 +44,36 @@ class Square:
         while not rospy.is_shutdown():
             d = self.get_distance()
             if d < self.sidelength:
+
                 self.twist.angular.z = 0
-                # keep driving forward
-                # get motor speed
+
+                # drive forwards
+                self.drive_straight()
+
+                # do some error stuff - should we do this within drive_straight()?
                 error = abs((d**2-self.sidelength**2))**0.5
                 self.motorSpeed = error
-                self.currentAngle = 0
+
+                # do some error checking
+                # threshold_angle = .5
+                # if (self.currentAngle > threshold
                 
                 
             else:
                 self.twist.linear.x = 0
                 # calculate the angle to increase by
-                self.twist.angular.z = 1
+                self.twist.angular.z = self.get_angular_v()
                 
                     
                 
-                # stop driving, turn right 90 deg, set new setpoint as current pos
+                # stop driving, turn right 90 deg, set new set_point as current pos
             self.pub.publish(self.twist)
             r.sleep()
         
-    def responseImu(self, message):
+    def response_imu(self, message):
         self.currentAngle = message.orientation.w
 
-    def response(self, message):
+    def response_odom(self, message):
         '''
         updates the current position class attribute
         '''
@@ -66,11 +83,13 @@ class Square:
 
     def get_distance(self):
         '''
-        compares the current position class attribute to the last setpoint to calculate distance from it
+        compares the current position class attribute to the last set_point to calculate distance from it
         '''
         # grab euclidean distance from starting point to current position 
-        return ((self.current_position[0]-self.setpoint[0])**2+(self.current_position[1]-self.setpoint[1])**2)**.5
+        return ((self.current_position[0]-self.set_point[0])**2+(self.current_position[1]-self.set_point[1])**2)**.5
 
+    def get_angular_v(self):
+        return( self.coef_a(90-self.currentAngle))
 
     def get_speed(self, distance_driven):
         speed = (self.sidelength-distance_driven)*(self.coef)
@@ -78,8 +97,10 @@ class Square:
         return(speed)
 
     def drive_straight(self, distance):
+        # calculate required speed
         speed = self.get_speed(distance)
-        self.twist = Twist(Vector3(speed,0,0), Vector3(0,0,0))
+        # set twist attribute based on speed, publish 
+        self.twist.linear.x = speed
         self.pub.publish(self.twist)
         
 
