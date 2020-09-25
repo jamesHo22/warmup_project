8bd0d8ad8ecbe7aaed0bb21acb9b6a47e331cb28
jamesho22 <8.james.ho@gmail.com>
2020-09-25 14:12:36 -0400

    add personFollower file

---
+++ warmup_neato/scripts/personFollower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# importing message stuff
+from std_msgs.msg import Int8MultiArray, Float32
+from geometry_msgs.msg import Twist, Vector3, Pose
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
+from tf.transformations import euler_from_quaternion
+
+import rospy
+import time
+import math
