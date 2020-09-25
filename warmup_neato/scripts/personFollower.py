35057d3320761b830ddc94dde1f28ca93d9a6f57
jamesho22 <8.james.ho@gmail.com>
2020-09-25 15:13:37 -0400

    POI

--- warmup_neato/scripts/personFollower.py
+++ warmup_neato/scripts/personFollower.py
@@ -4,22 +4,36 @@
 from std_msgs.msg import Int8MultiArray, Float32
 from geometry_msgs.msg import Twist, Vector3, Pose
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Imu
+from sensor_msgs.msg import Imu, LaserScan
 from tf.transformations import euler_from_quaternion
 
 import rospy
 import time
 import math
+import numpy as np
+
+class personFollwer:
+    def __init__(self):
+        # init node
+        rospy.init_node('personFollwer')
+
+        self.scanSub = rospy.Subscriber('/scan', LaserScan, self.recieveScan)
+        self.POI = (0,0) # (distance in meters, direction in radians) in polar coordinates in reference to the base link
+        self.lidarPoints = []
+        # driving
+        self.twist = Twist(Vector3(0,0,0), Vector3(0,0,0))
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+    def recieveScan(self, msg):
+        self.lidarPoints = msg.ranges
+    
+    def getPOI(self):
+        minDist = min(self.lidarPoints)
+        self.lidarPoints.index()
+        
 
 
 
-
-
-
-
-
-
-def move_towards(self, angle, distance):
-    # given an angle and a distance from the base_link frame, the neato should aim to 
-    # move in the right direction and close the gap. 
-    # The function should allow for recalibration (run off of ) 
+if __name__ == "__main__":
+    myPersonFollwer = personFollwer()
+    rospy.spin()
